ed6dc45cbcf60eaa298a455f6d268b8cdc2bc822
DolphaGo <adamdoha@naver.com>
2023-11-04 02:10:59 +0900

    build: upgradle gradle version

--- buildSrc/src/main/kotlin/JibConfigPlugin.kt
+++ buildSrc/src/main/kotlin/JibConfigPlugin.kt
@@ -1,6 +1,8 @@
 import com.google.cloud.tools.jib.gradle.JibExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.internal.impldep.com.amazonaws.util.StringUtils
+import org.gradle.internal.impldep.com.amazonaws.util.StringUtils.lowerCase
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.provideDelegate
 
@@ -9,7 +11,7 @@ class JibConfigPlugin : Plugin<Project> {
         project.pluginManager.withPlugin("com.google.cloud.tools.jib") {
             project.extensions.configure<JibExtension> {
                 val imageName: String by lazy {
-                    project.rootProject.name + project.path.replace(":", "/").toLowerCase()
+                    project.rootProject.name + lowerCase(project.path.replace(":", "/"))
                 }
                 val imageVersion: String? by project
                 val mainClassName: String by project
